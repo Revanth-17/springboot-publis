be885a9b2c8370c816ebeb7aed317fc087a0e236
Revanth Reddy <revanthreddypasham@gmail.com>
2025-07-30 19:51:28 +0530

    app properties changed

--- src/main/java/com/springboot/maven/publis/controller/PublisController.java
+++ src/main/java/com/springboot/maven/publis/controller/PublisController.java
@@ -15,44 +15,44 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.maven.publis.entity.UserEntity;
 import com.springboot.maven.publis.service.UserService;
 @RestController
-@RequestMapping("/users") // Base URL for all endpoints
+@RequestMapping("/users") 
 public class PublisController {
 
     @Autowired
     private UserService userService;
 
-    // CREATE
+
     @PostMapping
     public UserEntity createUser(@RequestBody UserEntity user) {
         return userService.createUser(user);
     }
 
-    // READ (Get user by ID)
+
     @GetMapping("/{id}")
     public UserEntity getUserById(@PathVariable Integer id) {
         return userService.getUserById(id);
     }
 
-    // READ (Get all users)
+
     @GetMapping
     public List<UserEntity> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    // UPDATE
+
     @PutMapping("/{id}")
     public UserEntity updateUser(@PathVariable Integer id, @RequestBody UserEntity user) {
         return userService.updateUser(id, user);
     }
 
-    // DELETE (by ID)
+
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable Integer id) {
         boolean deleted = userService.deleteUser(id);
         return deleted ? "User deleted successfully" : "User not found";
     }
 
-    // DELETE (all users)
+
     @DeleteMapping
     public String deleteAllUsers() {
         userService.deleteAllUsers();
